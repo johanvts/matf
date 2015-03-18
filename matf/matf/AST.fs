5b4d15b45dd536686a878d16b664ecebf29c1de3
Florian Biermann <fbie@itu.dk>
2015-03-18 21:53:22 +0100

    format sources
    Xamarin did this for me.

--- matf/matf/AST.fs
+++ matf/matf/AST.fs
@@ -2,24 +2,25 @@
 
 //Type abbriviations
 type label = string
+
 type identifier = string
 
 //Latex values
 type value = double
 
 //Latex operators
-type arithmetic =
-    |Add |Subtract |Multiply |Divide 
-
+type arithmetic = 
+    | Add
+    | Subtract
+    | Multiply
+    | Divide
 
 //Latex expression
-type expr =
-    |Literal of value
-    |Var of identifier
-    |Arithmetic of expr * arithmetic * expr
-    |Func of (value -> value)
-    |Sum of expr * expr * expr
-    |Prod of expr * expr * expr
-    |Frac of expr * expr
-    
-
+type expr = 
+    | Literal of value
+    | Var of identifier
+    | Arithmetic of expr * arithmetic * expr
+    | Func of (value -> value)
+    | Sum of expr * expr * expr
+    | Prod of expr * expr * expr
+    | Frac of expr * expr
