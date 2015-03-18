3b8ef19fdec946405f620ec49b908bdd7f63ea4f
unknown <jovt@itu.dk>
2015-03-18 18:15:39 +0100

    Remove old parsefloat and add matf 0.1. Includes some functionality but expecially the function handling could do with a critical reading.

---
+++ matf/matf/AST.fs
@@ -0,0 +1,25 @@
+﻿module AST
+
+//Type abbriviations
+type label = string
+type identifier = string
+
+//Latex values
+type value = double
+
+//Latex operators
+type arithmetic =
+    |Add |Subtract |Multiply |Divide 
+
+
+//Latex expression
+type expr =
+    |Literal of value
+    |Var of identifier
+    |Arithmetic of expr * arithmetic * expr
+    |Func of (value -> value)
+    |Sum of expr * expr * expr
+    |Prod of expr * expr * expr
+    |Frac of expr * expr
+    
+
---
+++ matf/matf/Playground.fsx
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "AST.fs"
+#load "Interpreter.fs"
+open AST
+open Interpreter
+open System.Collections.Generic
+// Define your library scripting code here
+
+let d = new Dictionary<identifier,value>()
+eval d (Arithmetic(Literal 12.0, Add,Literal 2.0))
+let square (a:double) =
+    a*a
+eval d (Sum(Literal 1.0,Literal 2.0,Func square))
